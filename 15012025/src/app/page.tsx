90e10c16d289e7dad7023132d15471998c6f7a9f
unknown <mm.choco.cornet.mm@gmail.com>
2025-01-24 00:19:50 +1000

    обновила блок под hero

--- 15012025/src/app/page.tsx
+++ 15012025/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from './components/hoc/Header';
 import Footer from './components/hoc/Footer';
 import Hero from './components/hoc/Hero';
+import Hero_Grid from './components/hoc/Hero_Grid';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Hero_Grid />
       <Footer />
     </div>
   );
